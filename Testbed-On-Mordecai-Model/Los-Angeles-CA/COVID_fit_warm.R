0d999f2d77b44337dd2d387caec4edb8cff70f6a
JiamingCui <37657654+JiamingCui@users.noreply.github.com>
2021-03-22 12:36:57 -0400

    Update COVID_fit_warm.R

--- Testbed-On-Mordecai-Model/Los-Angeles-CA/COVID_fit_warm.R
+++ Testbed-On-Mordecai-Model/Los-Angeles-CA/COVID_fit_warm.R
@@ -17,7 +17,7 @@ import_cases      <- FALSE    ## Use importation of cases?
 n.mif_runs        <- 2        ## mif2 fitting parameters
 n.mif_length      <- 300
 n.mif_particles   <- 600
-n.mif_rw.sd       <- 0.02
+n.mif_rw.sd       <- 0.002
 focal.county      <- "Los Angeles"  ## County to fit to
 ## !!! Curently parameters exist for Santa Clara, Miami-Dade, New York City, King, Los Angeles
 ## !!! But only Santa Clara explored
